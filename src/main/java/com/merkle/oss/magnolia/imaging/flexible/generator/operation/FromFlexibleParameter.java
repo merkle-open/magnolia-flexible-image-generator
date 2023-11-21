91712f2deef0778340aafa804a2dc09ef2b6144a
eschleb <bjoern.eschle@merkle.com>
2023-11-21 08:21:40 +0100

    Update dependencies

--- src/main/java/com/merkle/oss/magnolia/imaging/flexible/generator/operation/FromFlexibleParameter.java
+++ src/main/java/com/merkle/oss/magnolia/imaging/flexible/generator/operation/FromFlexibleParameter.java
@@ -7,14 +7,20 @@ import info.magnolia.imaging.operations.load.AbstractLoader;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FromFlexibleParameter extends AbstractLoader<ParameterProvider<FlexibleParameter>> {
 	@Override
 	protected BufferedImage loadSource(final ParameterProvider<FlexibleParameter> param) throws ImagingException {
 		try {
-			return doReadAndClose(param.getParameter().getContentStream());
+			return doReadAndClose(toInputStream(param));
 		} catch (IOException e) {
 			throw new ImagingException("Can't read image stream from flexibleParam.");
 		}
+	}
+
+	@Override
+	protected InputStream toInputStream(final ParameterProvider<FlexibleParameter> param) {
+		return param.getParameter().getContentStream();
 	}
 }
