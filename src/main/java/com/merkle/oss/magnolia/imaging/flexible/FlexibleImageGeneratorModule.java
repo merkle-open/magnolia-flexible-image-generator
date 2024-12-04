371bd9f2ac1ad20aec0fee59cdb810f694d70b98
eschleb <bjoern.eschle@merkle.com>
2024-12-04 17:31:02 +0100

    Simplify bundles parsing (path is not needed in pattern, since we are already "walking" only child-files)

--- src/main/java/com/merkle/oss/magnolia/imaging/flexible/FlexibleImageGeneratorModule.java
+++ src/main/java/com/merkle/oss/magnolia/imaging/flexible/FlexibleImageGeneratorModule.java
@@ -1,19 +1,23 @@
 package com.merkle.oss.magnolia.imaging.flexible;
 
-import com.google.inject.Provider;
-import com.merkle.oss.magnolia.imaging.flexible.model.bundle.ProcessedBundlesProvider;
 import info.magnolia.module.ModuleLifecycle;
 import info.magnolia.module.ModuleLifecycleContext;
+
+import java.lang.invoke.MethodHandles;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.merkle.oss.magnolia.imaging.flexible.model.bundle.ProcessedBundlesProvider;
 
 public class FlexibleImageGeneratorModule implements ModuleLifecycle {
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private final Provider<ProcessedBundlesProvider> bundlesProviderProvider;
 	private String bundlesConfigPath;
 
+	@Inject
 	public FlexibleImageGeneratorModule(final Provider<ProcessedBundlesProvider> bundlesProviderProvider){
 		this.bundlesProviderProvider = bundlesProviderProvider;
 	}
