8b3ec7a873cb0255192b744b23eeab5db3e2de66
eschleb <bjoern.eschle@merkle.com>
2026-01-07 10:18:58 +0100

    Improve ImageDigest hash creation performance

--- src/main/java/com/merkle/oss/magnolia/imaging/flexible/generator/uri/ImageDigest.java
+++ src/main/java/com/merkle/oss/magnolia/imaging/flexible/generator/uri/ImageDigest.java
@@ -7,16 +7,18 @@ import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import com.merkle.oss.magnolia.imaging.flexible.util.Lazy;
+
 public class ImageDigest {
 	static final String SALT_PROPERTY_KEY = "com.merkle.oss.magnolia.imaging.flexible.generator.uri.hash.salt";
 	private final Provider<String> saltProvider;
 
 	@Inject
 	public ImageDigest(final MagnoliaConfigurationProperties properties) {
-		this.saltProvider = () ->
-			Optional.ofNullable(properties.getProperty(SALT_PROPERTY_KEY)).orElseThrow(() ->
-					new NullPointerException("salt not configured! Set " + SALT_PROPERTY_KEY + " in magnolia.properties")
-			);
+		this.saltProvider = Lazy.of(() ->
+				Optional.ofNullable(properties.getProperty(SALT_PROPERTY_KEY)).orElseThrow(() ->
+						new NullPointerException("salt not configured! Set " + SALT_PROPERTY_KEY + " in magnolia.properties")
+				))::get;
 	}
 
 	public String getHash(final String input) {
